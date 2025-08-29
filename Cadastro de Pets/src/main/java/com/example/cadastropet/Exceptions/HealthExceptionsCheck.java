349867bf29c9cb24f36e99f1bbf7a118479c1bd9
Nathaandev <nathaanprogramador@hotmail.com>
2025-08-28 22:32:47 -0300

    Check if the reason for visit has invalid characters, but this time, it is permitted for the reason for visit have numbers.

--- Cadastro de Pets/src/main/java/com/example/cadastropet/Exceptions/HealthExceptionsCheck.java
+++ Cadastro de Pets/src/main/java/com/example/cadastropet/Exceptions/HealthExceptionsCheck.java
@@ -17,17 +17,25 @@ public class HealthExceptionsCheck {
         if (!cadastroRepository.existsById(healthRecordsModel.getPetId())) {
             throw new InvalidPetIDException("The pet ID does not exist.");
         }
+        
         if (healthRecordsModel.getVisitDate() == null) {
             throw new IllegalArgumentException("The date can't be null.");
         }
+
         if (healthRecordsModel.getDiagnosis() == null || healthRecordsModel.getDiagnosis().trim().isEmpty() || !healthRecordsModel.getDiagnosis().matches("[a-zA-Z ]*")) {
             throw new IllegalArgumentException("The diagnosis can't be empty or have numbers or special characters.");
         }
+
         if (healthRecordsModel.getVeterinaryName() == null || healthRecordsModel.getVeterinaryName().trim().isEmpty() || !healthRecordsModel.getVeterinaryName().matches("[a-zA-Z ]*")) {
             throw new IllegalArgumentException("The veterinary name can't be empty or have numbers or special characters.");
         }
+
         if (healthRecordsModel.getVeterinaryClinic() == null || healthRecordsModel.getVeterinaryClinic().trim().isEmpty() || !healthRecordsModel.getVeterinaryClinic().matches("[a-zA-Z ]*")) {
             throw new IllegalArgumentException("The veterinary clinic can't be empty or have numbers or special characters.");
+        }
+
+        if (healthRecordsModel.getReasonForVisit() == null || healthRecordsModel.getReasonForVisit().trim().isEmpty() || !healthRecordsModel.getReasonForVisit().matches("[a-zA-Z\\d ]*")) {
+            throw new IllegalArgumentException("The reason for visit can't be empty or have special characters.");
         }
     }
 }
